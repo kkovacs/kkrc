383daf376720cba8e18e40b002ecca5ad3293d1d
Kristof Kovacs <kkovacs@kkovacs.eu>
2018-02-10 20:14:50 +0100

    Last touches on uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 # Change to own dir
+curdir="$(pwd)"
 kkrcdir="$(dirname $0)"
 cd "$kkrcdir"
 
 # Installs the softlinks in place.
 process() {
-	local file="$1"
-	local softlink="$2"
+        local file="$1"
+        local softlink="$2"
 
-	echo "Removing softlink $file..."
+        echo "Removing softlink $file..."
 
-	if [ -L "$softlink" ]; then
-		echo "OK: Softlink. Removing."
-		rm "$softlink"
-	else
-		echo "WARNING: $softlink is not a softlink"
-	fi
+        if [ -L "$softlink" ]; then
+                echo "OK: Softlink. Removing."
+                rm "$softlink"
+        else
+                echo "WARNING: $softlink is not a softlink"
+        fi
 
-	# Just for pretty formatting
-	echo
+        # Just for pretty formatting
+        echo
 }
 
 # Process all files/dirs
@@ -28,15 +29,16 @@ process() {
 # .bashrc.orig is a special case, since that's where we moved the original.
 # Now rename it back
 if [ -f ~/.bashrc.orig ]; then
-	echo "OK: Renaming .bashrc.orig to .bashrc\n"
-	mv ~/.bashrc.orig ~/.bashrc
+        echo "OK: Renaming .bashrc.orig to .bashrc\n"
+        mv ~/.bashrc.orig ~/.bashrc
 else
-	echo "INFO: No .bashrc.orig found\n"
+        echo "INFO: No .bashrc.orig found\n"
 fi
 
 # Last step
 echo "Ready to run 'rm -rf $kkrcdir'? (y/n)"
 read A
 if [ "$A" = "y" ]; then
-	rm -rf "$kkrcdir"
+        cd "$curdir"
+        rm -rf "$kkrcdir"
 fi
