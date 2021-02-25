ec3cdd4eb2fe413e02f93d8cb34bf074df7e7328
kkovacs <kkovacs@Pendragon.local>
2021-02-25 15:52:30 +0100

    Removed --recursive from git submodule update for now

--- install.sh
+++ install.sh
@@ -41,5 +41,7 @@ echo "Updating git submodules:"
 if [ -e ~/.kkrc/.vim/pack/kkrc/start/vim-sensible/README.markdown ]; then
 	git submodule update --remote --merge
 else
-	git submodule update --recursive --init
+	git submodule update --init
+	# XXX Not using --recursive for now, because we don't need vim plugins' test-subrepos and stuff.
+	#WAS: git submodule update --recursive --init
 fi
