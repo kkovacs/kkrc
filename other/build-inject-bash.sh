d0cbc2bb6ef946b0d2af0789509ac3277559d5d0
kkovacs <kkovacs@Pendragon.local>
2022-01-29 10:15:12 +0100

    Added a clear to bash inject to make it less scary for others

--- other/build-inject-bash.sh
+++ other/build-inject-bash.sh
@@ -13,4 +13,4 @@ printf 'i\x7f HISTCONTROL=ignoreboth\r ' >../inject-bash.txt
 # XXX: The `sed` command is there to fix a "-bash: [: missing `]'" error.
 cat ../.bashrc | awk '/START of part to be injected/{on=1} {if (on) { print $0 }} /END of part to be injected/{on=0}' | python minifier.py | sed -e 's/)]/) ]/g' >>../inject-bash.txt
 # Add ENTER to minified wall-of-text, add commands for getting a quick status of the remote machine, then a final ENTER.
-printf '\r uptime;free -m;cat /etc/os-release;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
+printf '\r clear;uptime;free -m;cat /etc/os-release;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
