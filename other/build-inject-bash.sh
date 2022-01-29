a1a8a3f8920558248ea43af2a4eb0f62e07548ec
kkovacs <kkovacs@Pendragon.local>
2022-01-29 15:43:11 +0100

    better "clear" (still shows potential errors)

--- other/build-inject-bash.sh
+++ other/build-inject-bash.sh
@@ -8,9 +8,9 @@ cd "$(dirname $0)"
 # Enable ignore-lines-with-starting-space feature, while making as little a mess as possible.
 # Start with an "i" and a "DEL", in case remote is in VI-command mode.
 # Also, insert a starting space for the next wall-of-text.
-printf 'i\x7f HISTCONTROL=ignoreboth\r ' >../inject-bash.txt
+printf 'i\x7f HISTCONTROL=ignoreboth\r clear;' >../inject-bash.txt
 # Minify .bashrc's content between the two marker lines.
 # XXX: The `sed` command is there to fix a "-bash: [: missing `]'" error.
 cat ../.bashrc | awk '/START of part to be injected/{on=1} {if (on) { print $0 }} /END of part to be injected/{on=0}' | python minifier.py | sed -e 's/)]/) ]/g' >>../inject-bash.txt
 # Add ENTER to minified wall-of-text, add commands for getting a quick status of the remote machine, then a final ENTER.
-printf '\r clear;uptime;free -m;cat /etc/os-release;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
+printf '\r uptime;free -m;cat /etc/os-release;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
