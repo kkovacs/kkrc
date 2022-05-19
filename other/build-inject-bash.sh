d54beb3c0ecaafe56e0e831fb47f2429efaf096a
user <user@pc.local>
2022-05-19 20:51:12 +0200

    Better status screen

--- other/build-inject-bash.sh
+++ other/build-inject-bash.sh
@@ -13,4 +13,4 @@ printf 'i\x7f HISTCONTROL=ignoreboth\r clear;' >../inject-bash.txt
 # XXX: The `sed` command is there to fix a "-bash: [: missing `]'" error.
 cat ../.bashrc | awk '/START of part to be injected/{on=1} {if (on) { print $0 }} /END of part to be injected/{on=0}' | python3 minifier.py | sed -e 's/)]/) ]/g' >>../inject-bash.txt
 # Add ENTER to minified wall-of-text, add commands for getting a quick status of the remote machine, then a final ENTER.
-printf '\r uptime;free -m;cat /etc/os-release;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
+printf '\r getty --show-issue||cat /etc/os-release;uname -a;uptime;free -m;h 10;screen -ls 2>/dev/null;systemd-detect-virt;echo $SSH_AUTH_SOCK\r' >>../inject-bash.txt
