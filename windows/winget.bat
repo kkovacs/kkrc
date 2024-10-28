6413f55559649843f4f5b3ab657135ae7b468a6c
user <user@pc-2.local>
2024-10-28 15:24:13 +0100

    Initial version of winget.bat

---
+++ windows/winget.bat
@@ -0,0 +1,12 @@
+winget.exe update
+winget.exe install Microsoft.WindowsTerminal
+winget.exe install Microsoft.PowerToys
+winget.exe install Microsoft.VisualStudioCode
+winget.exe install Microsoft.OpenSSH.Beta
+winget.exe install Git.Git
+winget.exe install vim.vim
+winget.exe install Neovim.Neovim
+winget.exe install DuckDB.cli
+winget.exe install DuckDuckGo.DesktopBrowser
+winget.exe install MullvadVPN.MullvadBrowser
+winget.exe install KDE.Krita
