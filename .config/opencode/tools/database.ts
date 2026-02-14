56454258442b2a372c233de1c934a63cf7e74f3e
user <user@kk-e1>
2026-02-14 09:58:22 +0000

    Error handling for Opencode tool

--- .config/opencode/tools/database.ts
+++ .config/opencode/tools/database.ts
@@ -7,7 +7,19 @@ export default tool({
     sql: tool.schema.string().describe("SQL command"),
   },
   async execute(args, context) {
-    const result = await Bun.$`duckdb -c "${args.sql}" opencode-test.ddb`.text()
+    let result = "";
+
+    try {
+      result = await Bun.$`duckdb -c "${args.sql}" opencode-test.ddb`.text()
+    } catch (err) {
+      // For now, opencode didn't see the ShellError exception, so let's handle this for it.
+      // XXX Check https://opencode.ai/docs/custom-tools/ to see if they added error handling.
+      const stdout = err.stdout.toString();
+      const stderr = err.stderr.toString();
+
+      result = `Failed with code ${err.exitCode}\nSTDOUT: ${stdout}\nSTDERR: ${stderr}`;
+    }
+
     return result.trim()
   },
 })
