1d79428f0639b11f77801ff674096ef2da0b3b47
user <user@kk-e1>
2026-02-14 13:26:51 +0000

    Works and tested!

--- .config/opencode/tools/database.ts
+++ .config/opencode/tools/database.ts
@@ -10,14 +10,14 @@ export default tool({
     let result = "";
 
     try {
-      const result = await Bun.$`duckdb opencode-test.ddb`
-        .in$(args.sql)
+      // Should be safe, according to https://bun.com/docs/runtime/shell
+      result = await Bun.$`duckdb opencode-test.ddb ${args.sql}`
         .text();
     } catch (err) {
       // For now, opencode didn't see the ShellError exception, so let's handle this for it.
       // XXX Check https://opencode.ai/docs/custom-tools/ to see if they added error handling.
-      const stdout = err.stdout.toString();
-      const stderr = err.stderr.toString();
+      const stdout = err.stdout?.toString() ?? "";
+      const stderr = err.stderr?.toString() ?? "";
 
       result = `Failed with code ${err.exitCode}\nSTDOUT: ${stdout}\nSTDERR: ${stderr}`;
     }
